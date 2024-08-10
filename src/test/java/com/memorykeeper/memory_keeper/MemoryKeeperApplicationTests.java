401d70800a2e2c966dab3c5cbf3a835e7bde8117
wlgnsNam <wlgns8792@ajou.ac.kr>
2024-08-10 22:31:07 +0900

    Test2

--- src/test/java/com/memorykeeper/memory_keeper/MemoryKeeperApplicationTests.java
+++ src/test/java/com/memorykeeper/memory_keeper/MemoryKeeperApplicationTests.java
@@ -2,8 +2,10 @@ package com.memorykeeper.memory_keeper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MemoryKeeperApplicationTests {
 
 	@Test
