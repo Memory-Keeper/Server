7e4fa256ca145f5ed31854cccd76d22c28e784ce
park1NG <113297396+park1NG@users.noreply.github.com>
2024-08-11 00:43:05 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# 애플리케이션 실행 경로로 이동
-cd /home/ec2-user/app
+sudo yum install -y java-17-amazon-corretto-devel
+JARFILE=$(ls /home/ec2-user/app/*.jar)
+echo "filename : $JARFILE" >> startsh.log
 
-# 기존에 실행 중인 애플리케이션 프로세스를 종료
-sudo pkill -f 'java -jar'
+CURRENT_PID=$(pgrep -f $JARFILE)
+echo "currentpid : $CURRENT_PID" >> startsh.log
+kill -9 $CURRENT_PID 2>/dev/null
+sleep 5
 
-# 새 JAR 파일을 백그라운드에서 실행
-nohup java -jar memory-keeper-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
-
-# 새 프로세스의 PID를 기록
-echo $! > application.pid
+nohup java -jar $JARFILE > /dev/null 2> /dev/null < /dev/null &
